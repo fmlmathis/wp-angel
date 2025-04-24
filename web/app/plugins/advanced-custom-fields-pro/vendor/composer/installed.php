252046d26f7fc17db2808ab5aadac564bff7a60f
Mathis Fumel <fml.mathis@gmail.com>
2025-04-24 17:49:51 +0200

    TRYY

---
+++ web/app/plugins/advanced-custom-fields-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-engine/advanced-custom-fields',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '09935b9095e310af17a9daa8219127b4580fdfc3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-engine/advanced-custom-fields' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '09935b9095e310af17a9daa8219127b4580fdfc3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ web/app/plugins/polylang-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-engine/polylang-pro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '09935b9095e310af17a9daa8219127b4580fdfc3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-engine/polylang-pro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '09935b9095e310af17a9daa8219127b4580fdfc3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
